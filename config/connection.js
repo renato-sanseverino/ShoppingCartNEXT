948fbb4c2d2ae49f5e3ef1e62cb8f4c4b7973ad2
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-09-07 10:55:03 -0300

    Alterações na conexão com o banco

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
 
-const host     = 'localhost'
-const port     = 1433
-const username = 'sa'
-const password = 'p@ssw0rd'
+// dados da conexão com BD serverless
+const host     = 'us-east.connect.psdb.cloud'
+const port     = 3306
+const username = Buffer.from('OHMzdHlqNXk2dzA2bDQyY202cDM=', 'base64').toString('ascii')
+const password = Buffer.from('cHNjYWxlX3B3X1B6cVpoUzk5NGxJcmFaeGdyQzdwSmVrS1VuT3RocGNFZzFJV1lZblpDRTQ=', 'base64').toString('ascii')
 const database = 'commercedb'
-const ssl      = false
+const ssl      = true
 const setSSL   = 'sslaccept=strict&sslmode=require'
+const setUTF8  = 'useUnicode=true&characterEncoding=utf8&useSSL=false'
 
 
-// DATABASE_URL="sqlserver://localhost:1433;databaseName=commercedb;user=sa;password=p@ssw0rd;trustServerCertificate=true;"
-let url = `sqlserver://${host}:${port};databaseName=${database};user=${username};password=${password};trustServerCertificate=true;`;
+// DATABASE_URL="mysql://root:p@ssw0rd@localhost/commercedb"
+let url = `mysql://${username}:${password}@${host}/${database}`;
 if (ssl) {
     url = url + `?${setSSL}`;
 }
